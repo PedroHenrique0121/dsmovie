5f2f856c8d208cb6bea1d58b6642b0bee6cfd59f
PedroHenrique0121 <pedrohenriquegd2016@gmail.com>
2022-03-11 11:44:34 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -16,7 +16,7 @@ public class Movie {
     private Integer count;
     private String image;
 
-    @OneToMany(mappedBy = "id.movie")
+    @OneToMany(fetch = FetchType.EAGER,mappedBy = "id.movie")
     private Set<Score> scores = new HashSet<>();
 
     public Movie() {
