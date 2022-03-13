fd31e82094df9becad5878698103427a613486c9
PedroHenrique0121 <pedrohenriquegd2016@gmail.com>
2022-03-13 00:17:10 -0300

    correçao

--- frontend/src/componentes/Pages/Form/index.tsx
+++ frontend/src/componentes/Pages/Form/index.tsx
@@ -2,12 +2,10 @@
 import FormCard from "componentes/FormCard";
 import React from "react"
 import { Link, useLocation, useParams } from "react-router-dom";
-import { Movie } from "types/movie";
 
 
-type Props = {
-    movie: Movie;
-}
+
+
 function Form() {
     const params = useParams()
     const paramsL = useLocation()
